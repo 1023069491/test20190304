efdc4ecb115e69cb7d3288ff254d2d7fe5a17a73
lee bruce <1023069491@qq.com>
2018-06-28 23:41:29 +0800

    Initial Commit

---
+++ test0628/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test0628
+//
+//  Created by Lee on 2018/6/28.
+//  Copyright © 2018年 Dave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test0628/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test0628
+//
+//  Created by Lee on 2018/6/28.
+//  Copyright © 2018年 Dave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
