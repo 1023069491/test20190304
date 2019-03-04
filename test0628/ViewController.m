f920cad88c4cfa8c936993bcb4961802eb4d648e
lee bruce <1023069491@qq.com>
2019-03-04 22:29:20 +0800

    ce shi

---
+++ test0628/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  test0628
+//
+//  Created by Lee on 2018/6/28.
+//  Copyright © 2018年 Dave. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(void)test{
+    //NSLog(@"test0628");
+}
+
+-(void)mastAddSome{
+    NSLog(@"mastAddSome");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
